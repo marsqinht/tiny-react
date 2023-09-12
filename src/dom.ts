76325092c370a179c32bdb5e6235eaf9ba357387
qianhai <qianhai@bantouyan.com>
2023-09-12 21:29:38 +0800

    feat: 🎸 merge

--- src/dom.ts
+++ src/dom.ts
@@ -1,35 +1,4 @@
 
-export function appendChild(container: Element | Text, el: Element | Text) {
-  return container?.appendChild(el)
-}
-
-export function isInnerProperty(prop: string) {
-  return prop !== 'children' && prop !== 'nodeValue'
-}
-
-export function setDomAtrribute(el: Element, props: Record<string, any>, prop: string) {
-  if(prop === 'class') {
-    el.className = props[prop]
-  } else {
-    el[prop] = props[prop]
-  }
-}
-
-export function createDom(fiber) {
-  const dom =
-    fiber.type == "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(fiber.type)
-
-  const isProperty = key => key !== "children"
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach(name => {
-      dom[name] = fiber.props[name]
-    })
-
-  return dom
-}
 // export function createDom(fiber) {
 //   console.log('fiber :>> ', fiber);
 //   return document.createElement('div')
