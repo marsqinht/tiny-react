f293c96d9386705aacc16a0a86be1f2a3a342531
qianhai <qianhai@bantouyan.com>
2023-09-12 21:30:12 +0800

    feat: 🎸 ssss

--- src/dom.ts
+++ src/dom.ts
@@ -1,5 +1,32 @@
 
-// export function createDom(fiber) {
-//   console.log('fiber :>> ', fiber);
-//   return document.createElement('div')
-// }
+export function appendChild(container: Element | Text, el: Element | Text) {
+  return container?.appendChild(el)
+}
+
+export function isInnerProperty(prop: string) {
+  return prop !== 'children' && prop !== 'nodeValue'
+}
+
+export function setDomAtrribute(el: Element, props: Record<string, any>, prop: string) {
+  if(prop === 'class') {
+    el.className = props[prop]
+  } else {
+    el[prop] = props[prop]
+  }
+}
+
+export function createDom(fiber) {
+  const dom =
+    fiber.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(fiber.type)
+
+  const isProperty = key => key !== "children"
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = fiber.props[name]
+    })
+
+  return dom
+}
